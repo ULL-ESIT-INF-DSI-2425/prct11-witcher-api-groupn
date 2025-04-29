8342f37cdc51af62ab7e30ab0ae6e67341997383
Ismael Acosta Febles <alu0101323589@ull.edu.es>
2025-04-29 12:35:58 +0000

    async/await

--- src/routers/good.ts
+++ src/routers/good.ts
@@ -4,18 +4,25 @@ import { Good } from '../models/goods.js';
 export const goodRouter = express.Router();
 
 //post
-goodRouter.post('/goods', (req, res) => {
+goodRouter.post('/goods', async (req, res) => {
     const bien = new Good(req.body);
 
+    try{
+        await bien.save();
+        res.status(201).send(bien);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+    /*
     bien.save().then((bien) => {
         res.status(201).send(bien);
     }).catch((error) => {
         res.status(400).send(error);
-    });
+    });*/
 });
 
 //get
-goodRouter.get('/goods', (req, res) => {
+goodRouter.get('/goods', async (req, res) => {
 
     const allowedFilters = ['nombre', 'descripcion', 'material', 'peso', 'valor'];
     const actualFilters = Object.keys(req.query);
@@ -32,6 +39,17 @@ goodRouter.get('/goods', (req, res) => {
         });
     }
 
+    try {
+        const bienes = await Good.find(filters);
+        if (bienes.length !== 0) {
+            res.send(bienes);
+        } else {
+            res.status(404).send();
+        }
+    } catch (error) {
+        res.status(500).send(error);
+    }
+/*
     Good.find(filters).then((bienes) => {
         if(bienes.length !== 0) {
             res.send(bienes);   //todos los bienes (sin query: http://localhost:3000/goods)
@@ -40,10 +58,21 @@ goodRouter.get('/goods', (req, res) => {
         }
     }).catch(() => {
         res.status(500).send(); //error interno
-    });
+    });*/
 });
 
-goodRouter.get('/goods/:id', (req, res) => {
+goodRouter.get('/goods/:id', async (req, res) => {
+    try {
+        const bien = await Good.findById(req.params.id);
+        if (!bien) {
+            res.status(404).send();
+        } else {
+            res.send(bien);
+        }
+    } catch (error) {
+        res.status(500).send();
+    }
+    /*
     Good.findById(req.params.id).then((bien) => {
         if (!bien) {
             res.status(404).send();
@@ -52,12 +81,12 @@ goodRouter.get('/goods/:id', (req, res) => {
         }
     }).catch(() => {
         res.status(500).send();
-    });
+    });*/
 });
 
 
 //patch
-goodRouter.patch('/goods', (req, res) => {
+goodRouter.patch('/goods', async (req, res) => {
     if (!req.query.id) {
         res.status(400).send({error: 'An id must be provided in the query string',});
     } else if (!req.body) {
@@ -70,6 +99,22 @@ goodRouter.patch('/goods', (req, res) => {
         if (!isValidUpdate) {
             res.status(400).send({error: 'Update is not permitted',});
         } else {
+            try {
+
+                const bienUpdate = await Good.findOneAndUpdate({id: req.query.id}, req.body, {
+                    new: true,
+                    runValidators: true,
+                },);
+
+                if (bienUpdate) {
+                    res.send(bienUpdate);
+                } else {
+                    res.status(404).send();
+                }
+            } catch (error) {
+                res.status(500).send(error);
+            }
+            /*
             Good.findOneAndUpdate({id: req.query.id}, req.body, {
                 new: true,
                 runValidators: true,
@@ -81,12 +126,12 @@ goodRouter.patch('/goods', (req, res) => {
                 }
             }).catch((error) => {
                 res.status(400).send(error);
-            });
+            });*/
         }
     }
 });
 
-goodRouter.patch('/goods/:id', (req, res) => {
+goodRouter.patch('/goods/:id', async (req, res) => {
     if (!req.body) {
         res.status(400).send({error: 'Fields to be modified have to be provided in the request body',});
     } else {
@@ -97,6 +142,22 @@ goodRouter.patch('/goods/:id', (req, res) => {
         if (!isValidUpdate) {
             res.status(400).send({error: 'Update is not permited',});
         } else {
+
+            try {
+                const bien = await Good.findByIdAndUpdate(req.params.id, req.body, {
+                    new: true,
+                    runValidators: true,
+                });
+
+                if (!bien) {
+                    res.status(404).send();
+                } else {
+                    res.send(bien);
+                }
+            } catch (error) {
+                res.status(500).send(error);
+            }
+/*
             Good.findByIdAndUpdate(req.params.id, req.body, {
                 new: true,
                 runValidators: true,
@@ -108,17 +169,27 @@ goodRouter.patch('/goods/:id', (req, res) => {
                 }
             }).catch((error) => {
                 res.status(400).send(error);
-            });
+            });*/
         }
     }
 });
 
 //delete
-goodRouter.delete('/goods', (req, res) => {
+goodRouter.delete('/goods', async (req, res) => {
     if (!req.query.id) {
         res.status(400).send({error: 'An id must be provided',});
     } else {
-        Good.findOneAndDelete({id: req.query.id}).then((good) => {
+        try {
+            const bienDeleted = await Good.findOneAndDelete({id: req.query.id});
+            if (!bienDeleted) {
+                res.status(404).send();
+            } else {
+                res.send(bienDeleted);
+            }
+        } catch (error) {
+            res.status(500).send(error);
+        }
+       /* Good.findOneAndDelete({id: req.query.id}).then((good) => {
             if (!good) {
                 res.status(404).send();
             } else {
@@ -126,7 +197,7 @@ goodRouter.delete('/goods', (req, res) => {
             }
         }).catch(() => {
             res.status(400).send();
-        });
+        });*/
     }
 });
 
