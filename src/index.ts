c62d74f6559b8cc494af2b06eab79777395d7609
Alfredo Moreno <alu0101515601@ull.edu.es>
2025-05-06 21:40:09 +0000

    Add TSDoc comment to index.ts

--- src/index.ts
+++ src/index.ts
@@ -18,8 +18,16 @@ app.use(transactionRouter);
 app.use(defaultRouter);*/
 import {app} from "./app.js";
 
+/**
+ * Puerto en el que se ejecutará el servidor.
+ * Se utiliza la variable de entorno PORT si está definida, o el puerto 3000 por defecto.
+ */
 const port = process.env.PORT || 3000;
 
+/**
+ * Inicia el servidor y escucha en el puerto especificado.
+ * Muestra un mensaje en consola indicando que el servidor está activo.
+ */
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
